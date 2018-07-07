9cfd0ab5ca3a6c2b825ad562f71b1909098fc3ce
fengpengyuan <fengpengyuan@live.com>
2018-07-07 17:35:04 +0800

    111

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,15 @@ Route::group(['middleware' => 'auth'], function() {
         Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
         Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
         Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
+        Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
+        Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+        Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
+        Route::post('cart', 'CartController@add')->name('cart.add');
+        Route::get('cart', 'CartController@index')->name('cart.index');
+        Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
+        Route::post('orders', 'OrdersController@store')->name('orders.store');
+        Route::get('orders', 'OrdersController@index')->name('orders.index');
+        Route::get('orders/{order}', 'OrdersController@show')->name('orders.show');
     });
 });
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
