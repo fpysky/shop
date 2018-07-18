442d560e75055413f9875399e69d68fb9cd4fbe4
fengpengyuan <fengpengyuan@live.com>
2018-07-18 17:55:54 +0800

    111

--- database/admin.sql
+++ database/admin.sql
@@ -21,7 +21,7 @@
 
 LOCK TABLES `shop_admin_menu` WRITE;
 /*!40000 ALTER TABLE `shop_admin_menu` DISABLE KEYS */;
-INSERT INTO `shop_admin_menu` VALUES (1,0,1,'首页','fa-bar-chart','/',NULL,'2018-07-16 07:02:51'),(2,0,4,'系统设置','fa-tasks',NULL,NULL,'2018-07-17 06:41:15'),(3,2,5,'用户','fa-users','auth/users',NULL,'2018-07-17 06:41:15'),(4,2,6,'角色','fa-user','auth/roles',NULL,'2018-07-17 06:41:15'),(5,2,7,'权限','fa-ban','auth/permissions',NULL,'2018-07-17 06:41:15'),(6,2,8,'菜单','fa-bars','auth/menu',NULL,'2018-07-17 06:41:15'),(7,2,9,'操作日志','fa-history','auth/logs',NULL,'2018-07-17 06:41:15'),(8,0,3,'商品管理','fa-cubes','/products','2018-07-17 06:35:45','2018-07-17 06:41:15'),(9,0,2,'用户管理','fa-users','/users','2018-07-17 06:40:33','2018-07-17 06:41:15');
+INSERT INTO `shop_admin_menu` VALUES (1,0,1,'首页','fa-bar-chart','/',NULL,'2018-07-16 07:02:51'),(2,0,5,'系统设置','fa-tasks',NULL,NULL,'2018-07-18 08:20:00'),(3,2,6,'用户','fa-users','auth/users',NULL,'2018-07-18 08:20:00'),(4,2,7,'角色','fa-user','auth/roles',NULL,'2018-07-18 08:20:00'),(5,2,8,'权限','fa-ban','auth/permissions',NULL,'2018-07-18 08:20:00'),(6,2,9,'菜单','fa-bars','auth/menu',NULL,'2018-07-18 08:20:00'),(7,2,10,'操作日志','fa-history','auth/logs',NULL,'2018-07-18 08:20:00'),(8,0,3,'商品管理','fa-cubes','/products','2018-07-17 06:35:45','2018-07-17 06:41:15'),(9,0,2,'用户管理','fa-users','/users','2018-07-17 06:40:33','2018-07-17 06:41:15'),(10,8,4,'商品列表','fa-adn','/products','2018-07-18 08:19:21','2018-07-18 08:22:03'),(11,8,0,'商品分类','fa-align-justify','/productClassify','2018-07-18 08:28:28','2018-07-18 08:28:28');
 /*!40000 ALTER TABLE `shop_admin_menu` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -90,7 +90,7 @@ UNLOCK TABLES;
 
 LOCK TABLES `shop_admin_users` WRITE;
 /*!40000 ALTER TABLE `shop_admin_users` DISABLE KEYS */;
-INSERT INTO `shop_admin_users` VALUES (1,'admin','$2y$10$SS69EwdiTLfaPargtVlhL.m0VrYuykNDs5AvNkUYPZW2p.YaUJHWO','Administrator',NULL,NULL,'2018-07-16 06:55:20','2018-07-16 06:55:20');
+INSERT INTO `shop_admin_users` VALUES (1,'admin','$2y$10$SS69EwdiTLfaPargtVlhL.m0VrYuykNDs5AvNkUYPZW2p.YaUJHWO','Administrator',NULL,'kdfGhmrpuVoTSpq6ram2y9ywd25Yp9TNttAqae6cqNWRaYQQn43NtMYhfUkn','2018-07-16 06:55:20','2018-07-16 06:55:20');
 /*!40000 ALTER TABLE `shop_admin_users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -103,4 +103,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-07-18  2:30:59
+-- Dump completed on 2018-07-18  9:55:45
