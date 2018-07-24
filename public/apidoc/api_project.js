e2ee1e12a317a30df9f8599d6262d897f5f0f9bb
fengpengyuan <fengpengyuan@live.com>
2018-07-24 12:25:50 +0800

    1

---
+++ public/apidoc/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "商城",
+  "version": "0.1.0",
+  "description": "商城 RESTfull API文档",
+  "apidoc": "0.3.0",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-07-24T04:22:53.532Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
