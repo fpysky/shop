0de371e21a8e3c332b25aa372a34be5ed9d9c6e4
fengpengyuan <fengpengyuan@live.com>
2018-07-27 12:30:09 +0800

    1

--- routes/api/order.php
+++ routes/api/order.php
@@ -10,5 +10,17 @@ $api->version('v1', function ($api) {
         $api->post('orders','OrdersController@store');//创建订单
         $api->get('orders','OrdersController@orders');//创建订单
         $api->get('orders/{id}','OrdersController@show');//订单详情
+        $api->get('payment/{order}/alipay','PaymentController@payByAlipay');//订单支付
+        $api->get('payment/alipay/return','PaymentController@alipayReturn')->name('payment.alipay.return');//支付宝后端回调
+
     });
 });
+
+$api->version('v1', function ($api) {
+    $api->group([
+        'namespace' => 'App\Http\Controllers\Api\V1'
+    ], function ($api) {
+        $api->post('payment/alipay/notify','PaymentController@alipayNotify')->name('payment.alipay.notify');//支付宝前端回调
+    });
+});
+
