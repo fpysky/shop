ec5e7710518cc1b5cfbfa8305966c41c97c24515
fengpengyuan <fengpengyuan@live.com>
2018-07-30 11:15:39 +0800

    1

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,14 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/alipay', function() {
+Route::get('alipay', function() {
+//    dd(app('alipay'));
     return app('alipay')->web([
         'out_trade_no' => time(),
         'total_amount' => '1',
         'subject' => 'test subject - 测试',
     ]);
 });
+
+//Route::any('/payment/alipay/notify','Api\V1\PaymentController@alipayReturn')->name('payment.alipay.notify');//支付宝前端回调
+//Route::any('payment/alipay/return','Api\V1\PaymentController@alipayReturn')->name('payment.alipay.return');
