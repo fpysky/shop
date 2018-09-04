77ce590b43e372affe8ea4527b115ba8c4dc0624
fengpengyuan <fengpengyuan@live.com>
2018-09-04 14:34:19 +0800

    1

--- database/factories/ProductSkuAttributesFactory.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Faker\Generator as Faker;
-
-$factory->define(App\ProductSkuAttributes::class, function (Faker $faker) {
-    return [
-        //
-    ];
-});
--- resources/assets/js/components/Dashboard.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <h1>这里是已登陆的首页</h1>
-</template>
